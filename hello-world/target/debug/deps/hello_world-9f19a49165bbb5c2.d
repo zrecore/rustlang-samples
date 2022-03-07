7f3dd44c5e75c7ffc2a9c125ca92cab369f2f099
Alex Albino <webmaster@alexventure.com>
2022-03-07 11:38:40 -0800

    1.3 - Hello, Cargo

---
+++ hello-world/target/debug/deps/hello_world-9f19a49165bbb5c2.d
@@ -0,0 +1,5 @@
+/Users/aalbino/Projects/rustlang-samples/hello-world/target/debug/deps/hello_world-9f19a49165bbb5c2: src/main.rs
+
+/Users/aalbino/Projects/rustlang-samples/hello-world/target/debug/deps/hello_world-9f19a49165bbb5c2.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/aalbino/Projects/rustlang-samples/hello-world/target/debug/hello-world: /Users/aalbino/Projects/rustlang-samples/hello-world/src/main.rs
